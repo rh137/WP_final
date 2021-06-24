050cfb5818cbdfe68221f5ce8b28e9bbaafcf7a5
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-24 17:40:40 +0800

    style: rename function names in lowerCamelCase (sample frontend)

--- backend/src/public/index.js
+++ backend/src/public/index.js
@@ -6,54 +6,54 @@ server.sendEvent = (e) => server.send(JSON.stringify(e));
 server.onmessage = (m) => onEvent(JSON.parse(m.data));
 const onEvent = (e) => { alert(e.type) };
 
-const SignIn = () => {
+const signIn = () => {
   server.sendEvent({
     type: "SignIn",
     data: {}
   });
 }
-const SignUp = () => {
+const signUp = () => {
   server.sendEvent({
     type: "SignUp",
     data: {}
   })
 }
-const NewEvent = () => {
+const newEvent = () => {
   server.sendEvent({
     type: "NewEvent",
     data: {}
   })
 }
-const AddFriend = () => {
+const addFriend = () => {
   server.sendEvent({
     type: "AddFriend",
     data: {}
   })
 }
-const Invite = () => {
+const invite = () => {
   server.sendEvent({
     type: "Invite",
     data: {}
   })
 }
-const GetAvailableTimeSlots = () => {
+const getAvailableTimeSlots = () => {
   server.sendEvent({
     type: "GetAvailableTimeSlots",
     data: {}
   })
 }
-const UpdateAvailableTimeSlots = () => {
+const updateAvailableTimeSlots = () => {
   server.sendEvent({
     type: "UpdateAvailableTimeSlots",
     data: {}
   })
 }
-const RequestWithUnexpectedType = () => {
+const requestWithUnexpectedType = () => {
   server.sendEvent({
     type: "UnexpectedType",
     data: {}
   })
 }
-const RequestWithoutTypes = () => {
+const requestWithoutTypes = () => {
   server.sendEvent({})
 }
