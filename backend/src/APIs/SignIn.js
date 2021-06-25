f9c3c94d0c56447a8af96bf40a914fb6644c351d
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-25 15:02:25 +0800

    feat: `SignIn.getFriendObjectsByIds()` done

--- backend/src/APIs/SignIn.js
+++ backend/src/APIs/SignIn.js
@@ -1,5 +1,6 @@
 import db from "../db";
 import responses from "./commonFailedResponses";
+
 const { missingFieldsResponse, accountNotExistResponse } = responses;
 
 const signIn = async (args) => {
@@ -23,8 +24,8 @@ const signIn = async (args) => {
     data: {
       success: true,
       nickname: nickname,
-      friends: parseFriends(friends),
-      events: parseEvents(events)
+      friends: await getFriendObjectsByIds(friends),
+      events: getEventObjectsByIds(events)
     }
   }
 }
@@ -34,11 +35,21 @@ const hasMissingFields = (args) => {
   const { account, password } = args;
   return !account || !password;
 }
-const parseFriends = (friends) => {
-  // TODO: from _id to objects
-  return friends;
+const getFriendObjectsByIds = async (friendIds) => {
+  // ref: https://flaviocopes.com/javascript-async-await-array-map/
+  return Promise.all(
+    friendIds.map(
+      async (friendId) => {
+        const friend = await db.UserModel.findById(friendId);
+        return {
+          account: friend.account,
+          nickname: friend.nickname
+        };
+      }
+    )
+  );
 }
-const parseEvents = (events) => {
+const getEventObjectsByIds = (events) => {
   // TODO: from _id to objects
   return events;
 }
