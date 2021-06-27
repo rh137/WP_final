72c29034770ef6b0e846e4688ebc71c78945a8b4
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-27 23:52:32 +0800

    feat: new common response: NOT_INVITED

--- backend/src/APIs/commonFailedResponses/index.js
+++ backend/src/APIs/commonFailedResponses/index.js
@@ -2,12 +2,14 @@ import { accountExistsResponse } from "./accountExists";
 import { accountNotExistResponse } from "./accountNotExist";
 import { missingFieldsResponse } from "./missingFields";
 import { eventNotExistResponse } from "./eventNotExist";
+import { notInvitedResponse } from "./notInvitedResponse";
 
 const responses = {
   accountExistsResponse,
   accountNotExistResponse,
   missingFieldsResponse,
-  eventNotExistResponse
+  eventNotExistResponse,
+  notInvitedResponse
 };
 
 export default responses
