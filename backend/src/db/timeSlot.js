3c90e45f4b900fd35c7c1ad0b300a9268b62d3f6
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-27 23:54:58 +0800

    feat: new db model: TimeSlots

---
+++ backend/src/db/timeSlot.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const timeSlotSchema = new mongoose.Schema({
+  user: { type: mongoose.Types.ObjectId, ref: 'User' },
+  event: { type: mongoose.Types.ObjectId, ref: 'Event'},
+  date: Date,
+  startTime: Number,
+  endTime: Number
+});
+
+const timeSlotModel = mongoose.model('TimeSlot', timeSlotSchema);
+
+export default timeSlotModel;
