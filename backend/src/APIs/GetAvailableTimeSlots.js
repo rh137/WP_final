b846e6635c579e968964d2ad9330dbd2406a57d2
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-29 04:53:56 +0800

    feat: `GetAvailableTimeSlots()`: skeleton done

--- backend/src/APIs/GetAvailableTimeSlots.js
+++ backend/src/APIs/GetAvailableTimeSlots.js
@@ -1,5 +1,27 @@
+import db from "../db";
+
 const getAvailableTimeSlots = async (args) => {
   console.log('GetAvailableTimeSlots called.');
+
+  // TODO: check missing fields
+
+  // TODO: check if the event exists
+
+  // TODO: check if the account exists
+
+  // TODO: check if the user is invited
+
+  const { requesterAccount, eventId } = args;
+  //const user = await db.UserModel.findOne({ account: requesterAccount });
+  const event = await db.EventModel.findById(eventId);
+  const { startDate, endDate } = event;
+
+  // TODO: (pseudocode)
+  //    for date in [startDate, endDate]:
+  //      1) find all timeslots of event on date
+  //      2) apply parseTimeSlotsInADate.js
+  //      3) add date fields to each output time slot, push to ret
+  //    return ret
 
   return {
     type: "GetAvailableTimeSlots",
