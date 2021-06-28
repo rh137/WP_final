269edea1a10ccca46dd5f4801490b9dda6be59d6
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-29 04:43:22 +0800

    feat: `UpdateAvailableTimeSlots()` basically done (with 2 TODOs)

--- backend/src/APIs/UpdateAvailableTimeSlots.js
+++ backend/src/APIs/UpdateAvailableTimeSlots.js
@@ -1,6 +1,7 @@
 import db from "../db";
 import responses from "./commonFailedResponses";
-const { missingFieldsResponse, eventNotExistResponse, notInvitedResponse } = responses;
+const { missingFieldsResponse, eventNotExistResponse,
+        notInvitedResponse, accountNotExistResponse } = responses;
 
 const updateAvailableTimeSlots = async (args) => {
   console.log('UpdateAvailableTimeSlots called.');
@@ -16,6 +17,9 @@ const updateAvailableTimeSlots = async (args) => {
   }
 
   const user = await db.UserModel.findOne({account: requesterAccount});
+  if (!user){
+    return accountNotExistResponse("UpdateAvailableTimeSlots")
+  }
   if (!event.participants.includes(user._id)) {
     // TODO: check bi-directional relation?
     return notInvitedResponse("UpdateAvailableTimeSlots");
@@ -25,20 +29,38 @@ const updateAvailableTimeSlots = async (args) => {
 
   // TODO: async-lock the following block
   // update timeslot to db.TimeSlotModel
-
-  return {
-    type: "UpdateAvailableTimeSlots",
-    result: {},
-    data: {}
+  const { availableTimeSlots } = args;
+  await db.TimeSlotModel.deleteMany({user: user._id, event: event._id})
+  for (const { date, startTime, endTime } of availableTimeSlots) {
+    await new db.TimeSlotModel({
+      user: user._id,
+      event: event._id,
+      date: date,
+      startTime: startTime,
+      endTime: endTime
+    }).save();
   }
+
+  return updateAvailableTimeSlotsSuccessResponse();
 }
 
 // implementation details
 const hasMissingFields = (args) => {
-  const {requesterAccount, eventId, availableTimeSlots} = args;
-  return (!!requesterAccount || !eventId || !availableTimeSlots);
+  const { requesterAccount, eventId, availableTimeSlots } = args;
+  console.log(requesterAccount);
+  console.log(eventId);
+  console.log(availableTimeSlots);
+  return (!requesterAccount || !eventId || !availableTimeSlots);
 }
 
 // responses
+const updateAvailableTimeSlotsSuccessResponse = () => {
+  return {
+    type: "UpdateAvailableTimeSlots",
+    result: {
+      success: true
+    }
+  }
+}
 
 export default updateAvailableTimeSlots;
