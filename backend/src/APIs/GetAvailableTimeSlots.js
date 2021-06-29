c22ab561f876b29cba748cd90aaec0c033ac681b
rh137 <b06902137@csie.ntu.edu.tw>
2021-06-29 20:44:29 +0800

    feat: `GetAvailableTimeSlots()` done (with 4 TODOs)

--- backend/src/APIs/GetAvailableTimeSlots.js
+++ backend/src/APIs/GetAvailableTimeSlots.js
@@ -1,4 +1,6 @@
 import db from "../db";
+import { parseTimeSlots } from "./myUtil";
+import util from "util";
 
 const getAvailableTimeSlots = async (args) => {
   console.log('GetAvailableTimeSlots called.');
@@ -16,18 +18,77 @@ const getAvailableTimeSlots = async (args) => {
   const event = await db.EventModel.findById(eventId);
   const { startDate, endDate } = event;
 
-  // TODO: (pseudocode)
+  // (pseudocode)
   //    for date in [startDate, endDate]:
   //      1) find all timeslots of event on date
   //      2) apply parseTimeSlotsInADate.js
   //      3) add date fields to each output time slot, push to ret
   //    return ret
+  const ret = [];
+  const dateStrings = getAllDateStringsInRange(startDate, endDate);
+  for (const date of dateStrings) {
+    const timeSlotsOnDate = await db.TimeSlotModel.find({date: date, event: eventId});
+    const ret__ = await parseTimeSlots(timeSlotsOnDate)
+    if (ret__.length > 0) {
+      console.log(ret__[0].availableParticipants)
+      ret__.forEach((obj) => { obj.date = date })
+      ret.push(...ret__)
+    }
+  }
+  for (const r of ret) {
+    console.log(r)
+    //console.log(r.availableParticipants);
+  }
 
   return {
     type: "GetAvailableTimeSlots",
-    result: {},
-    data: {}
+    result: {
+      success: true
+    },
+    data: {
+      timeSlots: ret
+    }
   }
+}
+
+// implementation details
+const getDaysByYearAndMonth = (year, month) => {
+  switch (month) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
+    case 4: case 6: case 9: case 11: return 30;
+    case 2:
+      if (year % 400 === 0) return 29;
+      if (year % 100 === 0) return 28;
+      if (year % 4 === 0) return 29;
+      return 28;
+  }
+}
+function parseDateString(dateString) {
+  // dateString: type String, format yyyy_mm_dd
+  const [y, m, d] = dateString.split("_").map(s => parseInt(s, 10))
+  return { y: y, m: m, d: d }
+}
+function getAllDateStringsInRange(startDate, endDate) {
+  const start = parseDateString(startDate)
+  const end = parseDateString(endDate)
+  if (start.y  >  end.y) return []
+  if (start.y === end.y && start.m  >  end.m) return []
+  if (start.y === end.y && start.m === end.m && start.d > end.d) return []
+
+  const ret =[]
+  for (let y = start.y; y <= end.y; y++) {
+    for (let m = 1; m <= 12; m++) {
+      if (y === start.y && m < start.m) continue
+      if (y === end.y   && m > end.m) continue
+      const days = getDaysByYearAndMonth(y, m);
+      for (let d = 1; d <= days; d++) {
+        if (y === start.y && m === start.m && d < start.d) continue
+        if (y === end.y   && m === end.m   && d > end.d) continue
+        ret.push("" + y + "_" + (m < 10 ? "0" : "") + m + "_" + (d < 10 ? "0" : "") + d);
+      }
+    }
+  }
+  return ret;
 }
 
 export default getAvailableTimeSlots;
---
+++ backend/src/APIs/myUtil.js
@@ -0,0 +1,121 @@
+import util from 'util';
+import _ from 'lodash';
+import db from "../db";
+const logDeep = (obj) => { console.log(util.inspect(obj, false, null, true)); return;}
+
+const timeSlots = [
+  { account: "user1", startTime: 7, endTime: 9 },
+  { account: "user2", startTime: 8, endTime: 10 },
+]
+const expectedOutput = [
+  { startTime: 7, endTime: 8,  availableParticipants: [ { account: "user1" } ] },
+  { startTime: 8, endTime: 9,  availableParticipants: [ { account: "user1" }, { account: "user2" } ] },
+  { startTime: 9, endTime: 10, availableParticipants: [ { account: "user2" } ] }
+]
+async function parseTimeSlots (timeSlots) {
+  const splitTimeSlots = await split(timeSlots);
+  splitTimeSlots.sort(compareTimeSlots);
+
+  let ret = [];
+
+  let startTimeStack = []
+  let currentStartTime = -1;
+  let currentUsers = []
+  for (const ts of splitTimeSlots) {
+    if (ts.type === "START") {
+      startTimeStack.push(ts.time);
+      if (currentStartTime !== -1 && ts.time !== currentStartTime) {  // not first element
+        ret.push({
+          availableParticipants: Object.assign([], currentUsers),
+          startTime: currentStartTime,
+          endTime: ts.time,
+        })  // end last slot
+      }
+      currentStartTime = ts.time;
+      currentUsers.push(ts.user);
+    } else {  // ts.type === "END"
+      startTimeStack.pop();
+      if (ts.time !== currentStartTime) {
+        ret.push({
+          availableParticipants: Object.assign([], currentUsers),
+          startTime: currentStartTime,
+          endTime: ts.time,
+        })
+      }
+      // remove ts.account from currentUsers use filter
+      currentUsers = currentUsers.filter(obj => obj.account !== ts.account);
+
+      if (startTimeStack.length === 0) {
+        currentStartTime = -1  // restart
+      } else {
+        currentStartTime = ts.time;
+      }
+    }
+  }
+
+  return ret;
+}
+
+// utilities
+/*
+const timeSlots = [
+  { account: "user1", startTime: 7, endTime: 9 },
+  { account: "user2", startTime: 8, endTime: 10 },
+] */
+const expectedSplitTimeSlots = [
+  { account: "user1", time: 7, type: "START" },
+  { account: "user1", time: 9, type: "END" },
+  { account: "user2", time: 8, type: "START" },
+  { account: "user2", time: 10, type: "END" }
+]
+async function split (timeSlots) {
+  const splitTimeSlots = []
+  for (const ts of timeSlots) {
+    const user = await db.UserModel.findById(ts.user);
+    splitTimeSlots.push({
+      user: {
+        account: user.account,
+        nickname: user.nickname
+      },
+      // account: ts.account,
+      time: ts.startTime,
+      type: "START"
+    }, {
+      user: {
+        account: user.account,
+        nickname: user.nickname
+      },
+      // account: ts.account,
+      time: ts.endTime,
+      type: "END"
+    });
+  }
+  return splitTimeSlots;
+}
+function compareTypes (t1, t2) {
+  if (t1 < t2) return -1;
+  if (t1 > t2) return 1;
+  return 0;
+}
+function compareTimeSlots(ts1, ts2) {
+  if (ts1.time === ts2.time) {
+    return compareTypes(ts1.type, ts2.type);
+  } else {
+    return ts1.time - ts2.time;
+  }
+}
+
+
+function main () {
+  let o1 = parseTimeSlots(timeSlots);
+  let o2 = expectedOutput;
+
+  logDeep(o1)
+  logDeep(o2)
+
+  console.log(_.isEqual(o1, o2));
+}
+
+//main();
+
+export { parseTimeSlots };
